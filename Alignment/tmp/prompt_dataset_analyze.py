8e402a812939abd81dc7c071802a25a92051a916
paraGONG <1240138092@qq.com>
2024-09-19 22:30:23 +0800

    a

--- Alignment/tmp/prompt_dataset_analyze.py
+++ Alignment/tmp/prompt_dataset_analyze.py
@@ -1,4 +1,5 @@
 from datasets import load_dataset
+import json
 
 # 加载数据集
 dataset = load_dataset("OpenRLHF/prompt-collection-v0.1")
@@ -6,9 +7,11 @@ dataset = load_dataset("OpenRLHF/prompt-collection-v0.1")
 # 查看数据集的列
 print(dataset['train'].column_names)
 
+count = 0
 # 遍历 context_messages 列
 for i,data in enumerate(dataset['train']):
     context_messages = data['context_messages']
-    print(context_messages)
-    if i == 10:
-        break
+    objs = json.loads(context_messages)
+    if len(objs) > 1:
+        count += 1
+print(count)
