39e6cceeeb6782c081b6715d5c4841ae4d08bfa4
paraGONG <1240138092@qq.com>
2024-09-19 20:54:18 +0800

    check tokenizer

--- Alignment/tmp/check_tokenizer.py
+++ Alignment/tmp/check_tokenizer.py
@@ -7,3 +7,10 @@ tokenizer = AutoTokenizer.from_pretrained("PKU-Alignment/alpaca-7b-reproduced-ll
 vocab_size = tokenizer.vocab_size
 
 print(f"Tokenizer 词表大小: {vocab_size}")
+
+tokenizer = AutoTokenizer.from_pretrained("OpenRLHF/Llama-2-7b-rm-anthropic_hh-lmsys-oasst-webgpt")
+
+# 获取 tokenizer 的词表大小
+vocab_size = tokenizer.vocab_size
+
+print(f"Tokenizer 词表大小: {vocab_size}")
