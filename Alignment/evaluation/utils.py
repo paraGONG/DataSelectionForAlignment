b490898b6634ed49ab780431b973f1a4580aaf7a
paraGONG <1240138092@qq.com>
2024-09-20 21:05:16 +0800

    add eval

--- Alignment/evaluation/utils.py
+++ Alignment/evaluation/utils.py
@@ -545,12 +545,12 @@ class RewardPipeline:
             padding=padding,
             return_tensors="pt",
         ).to("cuda")
-        print(inputs.device)
+
         if self.tokenizer.bos_token:
             bos_token_id = self.tokenizer.bos_token_id
             input_ids = inputs["input_ids"]
             attention_mask = inputs["attention_mask"]
-
+            print(input_ids.device)
             # Ensure input_ids is 2D
             if input_ids.dim() == 1:
                 input_ids = input_ids.unsqueeze(0)
