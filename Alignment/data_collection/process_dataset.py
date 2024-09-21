1e47ca69418aebff00c9b11c358b4735f56e0bb8
paraGONG <1240138092@qq.com>
2024-09-21 21:38:17 +0800

    dataset

--- Alignment/data_collection/process_dataset.py
+++ Alignment/data_collection/process_dataset.py
@@ -3,14 +3,19 @@ import json
 
 dataset_path = 'stingning/ultrachat'
 dataset = load_dataset(dataset_path)
-dataset_name = dataset_path.spilt('/')[-1]
+dataset_name = dataset_path.split('/')[-1]
 
 output_file = f'../my_dataset/{dataset_name}.jsonl'
+seen_questions = set()
 
 with open(output_file, 'w', encoding='utf-8') as f:
     for row in dataset:
+        prompt = row['data'][0]
+        if prompt not in seen_questions:
+            # 如果是新问题，处理并保存
+            seen_questions.add(prompt)
         data = {
-            "prompt": row['data'][0],
+            "prompt": prompt,
             "source": dataset_name
         }
         f.write(json.dumps(data, ensure_ascii=False) + '\n')
