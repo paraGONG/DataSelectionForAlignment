b4e45c9cde5bd934062ee5c62d60c346513d3ac9
paraGONG <1240138092@qq.com>
2024-09-21 22:01:16 +0800

    dataset

--- Alignment/data_collection/process_dataset.py
+++ Alignment/data_collection/process_dataset.py
@@ -2,18 +2,19 @@ import os
 import json
 from datasets import load_dataset
 
-dataset_path = 'stingning/ultrachat'
+# dataset_path = 'stingning/ultrachat'
+dataset_path = 'nvidia/HelpSteer'
 dataset = load_dataset(dataset_path)
 dataset_name = dataset_path.split('/')[-1]
 
-output_file = f'../my_dataset/{dataset_name}.jsonl'
+output_file = f'../my_dataset/{dataset_name}-train.jsonl'
 os.makedirs('../my_dataset', exist_ok=True)
 
 seen_questions = set()
 
 with open(output_file, 'w', encoding='utf-8') as f:
     for row in dataset['train']:
-        prompt = row['data'][0]
+        prompt = row['prompt']
         if prompt not in seen_questions:
             seen_questions.add(prompt)
         data = {
