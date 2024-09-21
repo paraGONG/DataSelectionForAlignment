2646c5556f4cbd0545a8bbaff549b5b94e13c58a
paraGONG <1240138092@qq.com>
2024-09-21 23:51:37 +0800

    dataset

--- Alignment/data_collection/process_dataset.py
+++ Alignment/data_collection/process_dataset.py
@@ -1,23 +1,27 @@
 import os
 import json
 from datasets import load_dataset
-
-dataset_path = 'stingning/ultrachat'
+import re
+# dataset_path = 'stingning/ultrachat'
 # dataset_path = 'nvidia/HelpSteer'
 # dataset_path = 'openbmb/UltraInteract_pair'
 # dataset_path = 'PKU-Alignment/PKU-SafeRLHF'
-
+dataset_path = 'Anthropic/hh-rlhf'
 dataset = load_dataset(dataset_path)
 dataset_name = dataset_path.split('/')[-1]
 
-output_file = f'../my_dataset/{dataset_name}.jsonl'
+output_file = f'../my_dataset/{dataset_name}-train.jsonl'
 os.makedirs('../my_dataset', exist_ok=True)
 
 seen_questions = set()
 
+pattern = r"Human: (.*?) Assistant"
+
 with open(output_file, 'w', encoding='utf-8') as f:
     for row in dataset['train']:
-        prompt = row['data'][0]
+        prompt = row['chosen']
+        match = re.search(pattern, prompt, re.DOTALL)
+        prompt = match.group(1)
         if prompt not in seen_questions:
             seen_questions.add(prompt)
             data = {
