d33e4a9a89174144c718b491ed4256b84002a47a
paraGONG <1240138092@qq.com>
2024-09-22 22:27:56 +0800

    try diff settings

---
+++ Alignment/scripts/try_different_settings/train_ppo_tinyllamasft_tinyllamarm_actor_lora_llr_critic_lora_llr copy.sh
@@ -0,0 +1,34 @@
+deepspeed --module openrlhf.cli.train_ppo \
+  --pretrain  TinyLlama/TinyLlama-1.1B-Chat-v1.0 \
+  --reward_pretrain yifangong/TinyLlama-1.1B-Chat-v1.0-reward-model \
+  --save_path ../checkpoint/tinyllamasft-tinyllamarm-rlhf-actor-lora-llr-critic-lora-llr \
+  --ckpt_path  ../ckpt/tinyllamasft-tinyllamarm-rlhf-actor-lora-llr-critic-lora-llr \
+  --save_steps 10 \
+  --max_ckpt_num 500 \
+  --logging_steps 1 \
+  --eval_steps -1 \
+  --micro_train_batch_size 2 \
+  --train_batch_size 128 \
+  --micro_rollout_batch_size 4 \
+  --rollout_batch_size 512 \
+  --max_epochs 2 \
+  --prompt_max_len 1024 \
+  --generate_max_len 1024 \
+  --zero_stage 2 \
+  --bf16 \
+  --actor_learning_rate 5e-4 \
+  --critic_learning_rate 5e-4 \
+  --init_kl_coef 0.01 \
+  --prompt_data yifangong/rlhf-prompt-collection-v1.0 \
+  --input_key prompt \
+  --input_template "<|user|>\n{}</s>\n<|assistant|>\n" \
+  --max_samples 100000 \
+  --load_checkpoint \
+  --actor_lora_rank 8 \
+  --actor_lora_alpha 16 \
+  --critic_lora_rank 8 \
+  --critic_lora_alpha 16 \
+  --flash_attn \
+  --use_wandb b7f573ca98ce546e2a92a20e0602f5fb456156f2 \
+  --wandb_project try_tinyllamasft-tinyllamarm-rlhf-actor-lora-llr-critic-lora-llr \
+  --wandb_run_name try_tinyllamasft-tinyllamarm-rlhf-actor-lora-llr-critic-lora-llr
