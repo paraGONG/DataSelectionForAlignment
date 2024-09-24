50983a664cea3d6c74d35d16fee94bb000196998
paraGONG <1240138092@qq.com>
2024-09-24 19:51:54 +0800

    data

--- Alignment/data_collection/sample.py
+++ Alignment/data_collection/sample.py
@@ -5,8 +5,8 @@ data = pd.read_json('../rlhf_prompt_collection_5w.jsonl', lines=True)
 
 sampled_data = data.sample(n=10240, random_state=42)
 
-sampled_data.to_json('warmup_dataset.jsonl', orient='records', lines=True)
+sampled_data.to_json('../warmup_dataset.jsonl', orient='records', lines=True)
 
 # 保存剩余数据
 remaining_data = data.drop(sampled_data.index)
-remaining_data.to_json('candidata_dataset.jsonl', orient='records', lines=True)
+remaining_data.to_json('../candidata_dataset.jsonl', orient='records', lines=True)
