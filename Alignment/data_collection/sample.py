26a65978fb7068440f6f3f8bcad6bc0e064c0270
paraGONG <1240138092@qq.com>
2024-09-25 02:31:34 +0800

    new exp

--- Alignment/data_collection/sample.py
+++ Alignment/data_collection/sample.py
@@ -1,9 +1,9 @@
 import json
 import random
 
-input_file = 'rlhf_prompt_collection_5w.jsonl'
-selected_file = 'warmup_dataset.jsonl'
-remaining_file = 'candidate_dataset.jsonl'
+input_file = 'candidate_dataset.jsonl'
+selected_file = 'random_dataset.jsonl'
+# remaining_file = 'candidate_dataset.jsonl'
 num_samples = 10240
 
 # 读取原始数据
@@ -12,7 +12,7 @@ with open(input_file, 'r') as f:
 
 # 随机挑选数据
 selected_data = random.sample(data, min(num_samples, len(data)))
-remaining_data = [item for item in data if item not in selected_data]
+# remaining_data = [item for item in data if item not in selected_data]
 
 # 保存选中的数据
 with open(selected_file, 'w') as f:
@@ -20,6 +20,6 @@ with open(selected_file, 'w') as f:
         f.write(json.dumps(item) + '\n')
 
 # 保存剩余的数据
-with open(remaining_file, 'w') as f:
-    for item in remaining_data:
-        f.write(json.dumps(item) + '\n')
+# with open(remaining_file, 'w') as f:
+#     for item in remaining_data:
+#         f.write(json.dumps(item) + '\n')
