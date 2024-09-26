f3a34981a6a1ef6ffa82e6c367d0acc4679f1dc6
paraGONG <1240138092@qq.com>
2024-09-26 10:41:14 +0800

    add

--- DataSelection/compute_influence.py
+++ DataSelection/compute_influence.py
@@ -22,19 +22,19 @@ def prepare_gradients_train():
         gradients_path = f"../tinyllamachat_global_step10_gradients_train/tinyllamachat_global_step10_gradients_train_part_0-3/tinyllamachat_global_step10_gradients_train_part_{i}/gradients"
         num = len([f for f in os.listdir(gradients_path)])
         for j in range(num):
-            gradients = torch.load(os.path.join(gradients_path, f"gradient_{j+1}.pt", map_location="cuda:1"))
+            gradients = torch.load(os.path.join(gradients_path, f"gradient_{j+1}.pt"), map_location="cuda:1")
             gradients_train.append(gradients)
     for i in range(4):
         gradients_path = f"../tinyllamachat_global_step10_gradients_train/tinyllamachat_global_step10_gradients_train_part_4-7/tinyllamachat_global_step10_gradients_train_part_{i+4}/gradients"
         num = len([f for f in os.listdir(gradients_path)])
         for j in range(num):
-            gradients = torch.load(os.path.join(gradients_path, f"gradient_{j+1}.pt", map_location="cuda:1"))
+            gradients = torch.load(os.path.join(gradients_path, f"gradient_{j+1}.pt"), map_location="cuda:1")
             gradients_train.append(gradients)
     for i in range(2):
         gradients_path = f"../tinyllamachat_global_step10_gradients_train/tinyllamachat_global_step10_gradients_train_part_8-9/tinyllamachat_global_step10_gradients_train_part_{i+8}/gradients"
         num = len([f for f in os.listdir(gradients_path)])
         for j in range(num):
-            gradients = torch.load(os.path.join(gradients_path, f"gradient_{j+1}.pt", map_location="cuda:1"))
+            gradients = torch.load(os.path.join(gradients_path, f"gradient_{j+1}.pt"), map_location="cuda:1")
             gradients_train.append(gradients)
     return gradients_train
 
