bec1545e0b93fea64de9e9a178b12f1f47855c4e
paraGONG <1240138092@qq.com>
2024-09-26 08:47:24 +0800

    select

--- DataSelection/tmp.py
+++ DataSelection/tmp.py
@@ -1,6 +1,6 @@
 import torch
 
-data = torch.load('.pt')
+data = torch.load('../tinyllamachat_global_step10_gradients_train/tinyllamachat_global_step10_gradients_train_part_0-3/tinyllamachat_global_step10_gradients_train_part_0/gradients/gradient_0.pt')
 
 if isinstance(data, dict):
     for key, value in data.items():
