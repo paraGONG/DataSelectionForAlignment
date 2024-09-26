b6b82585d75547cb52c40fd80772b54a6039b607
paraGONG <1240138092@qq.com>
2024-09-26 09:00:41 +0800

    select

--- DataSelection/tmp.py
+++ DataSelection/tmp.py
@@ -19,11 +19,10 @@ def calculate_influence_score(training_info: torch.Tensor, validation_info: torc
     """
     # N x N_VALID
     influence_scores = torch.matmul(
-        training_info, validation_info.transpose(0, 1))
+        training_info, validation_info)
     return influence_scores
 
 data1 = torch.load('../tinyllamachat_global_step10_gradients_train/tinyllamachat_global_step10_gradients_train_part_0-3/tinyllamachat_global_step10_gradients_train_part_0/gradients/gradient_1.pt')
 data2 = torch.load('../tinyllamachat_global_step10_gradients_train/tinyllamachat_global_step10_gradients_train_part_0-3/tinyllamachat_global_step10_gradients_train_part_0/gradients/gradient_2.pt')
 influence_score = calculate_influence_score(data1, data2)
-print(influence_score.shape)
 print(influence_score)
