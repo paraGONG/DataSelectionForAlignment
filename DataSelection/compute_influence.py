031a26d07e670a0104e2531996e25d14ae4440b0
paraGONG <1240138092@qq.com>
2024-09-26 09:27:04 +0800

    select

---
+++ DataSelection/compute_influence.py
@@ -0,0 +1,12 @@
+import os
+import json
+import torch
+import tqdm
+
+def prepare_gradients_train():
+    gradients_train = []
+    for i in range(4):
+        gradients_path = f"tinyllamachat_global_step10_gradients_train/tinyllamachat_global_step10_gradients_train_part_0-3/tinyllamachat_global_step10_gradients_train_part_{i}/gradients"
+        num = len([f for f in os.listdir(gradients_path)])
+        print(num)
+
