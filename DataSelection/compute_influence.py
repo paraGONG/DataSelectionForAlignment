12dd3d9825b0a7fdba0759c7467a98879c104d87
paraGONG <1240138092@qq.com>
2024-09-26 09:34:02 +0800

    select

--- DataSelection/compute_influence.py
+++ DataSelection/compute_influence.py
@@ -8,6 +8,17 @@ def prepare_gradients_train():
     for i in range(4):
         gradients_path = f"../tinyllamachat_global_step10_gradients_train/tinyllamachat_global_step10_gradients_train_part_0-3/tinyllamachat_global_step10_gradients_train_part_{i}/gradients"
         num = len([f for f in os.listdir(gradients_path)])
-        print(num)
-
+        for j in range(num):
+            gradients_train.append(os.path.join(gradients_path, f"gradient_{j+1}.pt"))
+    for i in range(4):
+        gradients_path = f"../tinyllamachat_global_step10_gradients_train/tinyllamachat_global_step10_gradients_train_part_4-7/tinyllamachat_global_step10_gradients_train_part_{i+4}/gradients"
+        num = len([f for f in os.listdir(gradients_path)])
+        for j in range(num):
+            gradients_train.append(os.path.join(gradients_path, f"gradient_{j+1}.pt"))
+    for i in range(2):
+        gradients_path = f"../tinyllamachat_global_step10_gradients_train/tinyllamachat_global_step10_gradients_train_part_48-9/tinyllamachat_global_step10_gradients_train_part_{i+8}/gradients"
+        num = len([f for f in os.listdir(gradients_path)])
+        for j in range(num):
+            gradients_train.append(os.path.join(gradients_path, f"gradient_{j+1}.pt"))
+    print(len(gradients_path))
 prepare_gradients_train()
