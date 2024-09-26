b884b4c5eed62c4bf2096fee407c57c189de5772
paraGONG <1240138092@qq.com>
2024-09-26 10:22:27 +0800

    add

--- DataSelection/compute_influence.py
+++ DataSelection/compute_influence.py
@@ -3,6 +3,19 @@ import json
 import torch
 import tqdm
 
+def calculate_influence_score(training_info: torch.Tensor, validation_info: torch.Tensor):
+    """Calculate the influence score.
+
+    Args:
+        training_info (torch.Tensor): training info (gradients/representations) stored in a tensor of shape N x N_DIM
+        validation_info (torch.Tensor): validation info (gradients/representations) stored in a tensor of shape N_VALID x N_DIM
+    """
+    # N x N_VALID
+    influence_scores = torch.matmul(
+        training_info, validation_info)
+    return influence_scores.item()
+
+
 def prepare_gradients_train():
     gradients_train = []
     for i in range(4):
@@ -20,5 +33,28 @@ def prepare_gradients_train():
         num = len([f for f in os.listdir(gradients_path)])
         for j in range(num):
             gradients_train.append(os.path.join(gradients_path, f"gradient_{j+1}.pt"))
-    print(len(gradients_train))
-prepare_gradients_train()
+    return gradients_train
+
+
+def prepare_gradients_evaluation():
+    gradients_eval = []
+    gradients_path = f"../tinyllamachat_global_step10_gradients_evaluation_saferlhf/gradients"
+    num = len([f for f in os.listdir(gradients_path)])
+    for j in range(num):
+        gradients_eval.append(os.path.join(gradients_path, f"gradient_{j+1}.pt"))
+    return gradients_eval
+
+
+def compute_influence(gradients_train, gradients_eval, save_path):
+    os.makedirs(save_path, exist_ok=True)
+    for i, eval_gradient in enumerate(gradients_eval):
+        influence_scores = []
+        for train_gradient in tqdm(train_gradient):
+            influence_score = calculate_influence_score(train_gradient, eval_gradient)
+            influence_scores.append(influence_score)
+        with open(os.path.join(save_path, f"scores_{i}"), 'w') as f:
+            json.dump(influence_scores, f)
+
+gradients_train = prepare_gradients_train()
+gradients_eval = prepare_gradients_evaluation()
+compute_influence(gradients_train, gradients_eval, "../influence")
