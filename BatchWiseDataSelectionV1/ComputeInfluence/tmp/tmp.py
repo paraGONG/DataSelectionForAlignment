1c44c284c62da4061487d7934b83ef90648d451c
paraGONG <1240138092@qq.com>
2024-12-13 01:30:53 +0800

    debug

--- BatchWiseDataSelectionV1/ComputeInfluence/tmp/tmp.py
+++ BatchWiseDataSelectionV1/ComputeInfluence/tmp/tmp.py
@@ -1,4 +1,4 @@
 import torch
-items = torch.load('./buffer/window_0/device_0/output/buffer_items.pth')
+items = torch.load('../buffer/window_0/device_0/output/buffer_items.pth')
 
 print(len(items))
