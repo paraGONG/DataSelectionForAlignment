ecaa812cc21219694d225afc4017d23e5843b0cc
paraGONG <1240138092@qq.com>
2024-12-13 03:30:15 +0800

    debug

--- BatchWiseDataSelectionV1/ComputeInfluence/openrlhf/trainer/gradient_calculator.py
+++ BatchWiseDataSelectionV1/ComputeInfluence/openrlhf/trainer/gradient_calculator.py
@@ -378,7 +378,7 @@ class GradientCalculator(ABC):
 
         loss = actor_loss
         # self.strategy.backward(loss, self.actor, self.actor_optim)
-        self.actor_optim.backward(loss, self.actor, self.actor_optim)
+        self.strategy.backward(loss, self.actor, self.actor_optim)
         # save gradient
         vectorized_grads = torch.cat([p.grad.view(-1) for p in self.actor.parameters() if p.grad is not None])
         print(vectorized_grads)
