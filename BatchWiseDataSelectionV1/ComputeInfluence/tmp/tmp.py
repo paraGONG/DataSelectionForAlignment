9360ee219b2e9e8638ac5f87a600546bc6c18063
paraGONG <1240138092@qq.com>
2024-12-13 01:35:09 +0800

    debug

--- BatchWiseDataSelectionV1/ComputeInfluence/tmp/tmp.py
+++ BatchWiseDataSelectionV1/ComputeInfluence/tmp/tmp.py
@@ -1,4 +1,23 @@
+import math
+import json
+import os.path
+from abc import ABC
+from typing import Any, Callable, Dict, List, Optional, Union
+
+import ray
 import torch
+import torch.nn as nn
+from torch import Tensor
+from torch.optim import Optimizer
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from openrlhf.models import Actor, GPTLMLoss, PolicyLoss, ValueLoss
+from openrlhf.models.utils import masked_mean
+from openrlhf.utils.distributed_sampler import DistributedSampler
+
+from .ppo_utils import AdaptiveKLController, Experience, FixedKLController, NaiveExperienceMaker, NaiveReplayBuffer
+
 items = torch.load('../buffer/window_0/device_0/output/buffer_items.pth')
 
 print(len(items))
