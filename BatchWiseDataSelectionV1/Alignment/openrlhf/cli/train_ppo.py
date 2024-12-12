f4d681b928e22813661f7973b3fdb3962269bebe
paraGONG <1240138092@qq.com>
2024-12-13 05:29:39 +0800

    debug

--- BatchWiseDataSelectionV1/Alignment/openrlhf/cli/train_ppo.py
+++ BatchWiseDataSelectionV1/Alignment/openrlhf/cli/train_ppo.py
@@ -189,7 +189,7 @@ def train(args):
         ema_beta=0.992,
         ptx_coef=args.ptx_coef,
         max_norm=args.max_norm,
-        items_path=os.path.join(args.buffer_path, f"window_{args.window_num}", f"device_{torch.cuda.current_device()}", "items", f"{args.select_policy}_items"),
+        items_path=os.path.join(args.buffer_path, f"window_{args.window_num}", f"device_{torch.cuda.current_device()}", "items", f"{args.select_policy}_items.pth"),
         # fro GPT generation
         do_sample=True,
         max_new_tokens=args.generate_max_len,
