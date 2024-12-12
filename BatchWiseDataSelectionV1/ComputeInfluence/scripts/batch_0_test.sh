05453fb00e2b10a4ebb9025ef81dfed5cd5fa643
paraGONG <1240138092@qq.com>
2024-12-13 00:17:34 +0800

    test

--- BatchWiseDataSelectionV1/ComputeInfluence/scripts/batch_0_test.sh
+++ BatchWiseDataSelectionV1/ComputeInfluence/scripts/batch_0_test.sh
@@ -1,4 +1,4 @@
-deepspeed --include localhost:0 --master_port 29500 --hostfile=None --module openrlhf.cli.calculate_gradient \
+deepspeed --master_port 29500 --hostfile=None --module openrlhf.cli.calculate_gradient \
   --pretrain  TinyLlama/TinyLlama-1.1B-Chat-v1.0 \
   --reward_pretrain yifangong/TinyLlama-1.1B-Chat-v1.0-reward-model \
   --save_path ../buffer \
