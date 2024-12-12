32250e255fbd2ac3c786090e2c54f60c2ddf5c13
paraGONG <1240138092@qq.com>
2024-12-13 06:46:14 +0800

    debug

---
+++ BatchWiseDataSelectionV1/Alignment/scripts/new_exp/batch_0.sh
@@ -0,0 +1,31 @@
+deepspeed --module --hostfile=None openrlhf.cli.train_ppo \
+  --pretrain  TinyLlama/TinyLlama-1.1B-Chat-v1.0 \
+  --reward_pretrain yifangong/TinyLlama-1.1B-Chat-v1.0-reward-model \
+  --save_path ../checkpoint/tinyllama \
+  --ckpt_path  ../../../tinyllama-warmup-ckpt \
+  --ckpt_tag global_step10 \
+  --save_steps 1 \
+  --max_ckpt_num 1000 \
+  --logging_steps 1 \
+  --eval_steps -1 \
+  --micro_train_batch_size 4 \
+  --train_batch_size 128 \
+  --micro_rollout_batch_size 8 \
+  --rollout_batch_size 128 \
+  --max_epochs 1 \
+  --prompt_max_len 1024 \
+  --generate_max_len 1024 \
+  --zero_stage 0 \
+  --bf16 \
+  --actor_learning_rate 5e-4 \
+  --critic_learning_rate 5e-4 \
+  --init_kl_coef 0.01 \
+  --max_samples 100000 \
+  --load_checkpoint \
+  --actor_lora_rank 8 \
+  --actor_lora_alpha 16 \
+  --critic_lora_rank 8 \
+  --critic_lora_alpha 16 \
+  --window_num 0 \
+  --select_policy chosen \
+  --buffer_path ../buffer
