a9c466f9c2e56813c03ccbb480ddeb4b529882c3
paraGONG <1240138092@qq.com>
2025-01-10 10:24:54 +0800

    add

--- BatchWiseDataSelectionV3/openrlhf/trainer/ppo_trainer.py
+++ BatchWiseDataSelectionV3/openrlhf/trainer/ppo_trainer.py
@@ -277,7 +277,8 @@ class PPOTrainer(ABC):
             )
             for i, experience in enumerate(pbar):
                 experience.to_device(device)
-
+                print("threshold: ", threshold)
+                print("i: ", i)
                 # training step
                 # status = self.training_step(experience, global_steps)
                 status = {}
