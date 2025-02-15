cb0cd9e11f8cba34450faf59909d628c2ec88987
paraGONG <1240138092@qq.com>
2025-02-15 17:37:39 +0800

    case study

--- BatchWiseDataSelectionV3_case/openrlhf/trainer/ppo_trainer.py
+++ BatchWiseDataSelectionV3_case/openrlhf/trainer/ppo_trainer.py
@@ -645,10 +645,6 @@ class PPOTrainer(ABC):
         with open(os.path.join(self.output_save_path, f'steps_{steps}_high_inf.jsonl'), "w") as f:
             for influence, item, loss in top_20_data:
                 output = self.tokenizer.batch_decode(item.sequences, skip_special_tokens=True)
-                print(type(item.info["reward"]))
-                print(type(item.returns))
-                print(type(item.advantages))
-                print(type(item.values))
                 json_obj = {
                     "data" : 'high_inf', 'output': output[0], 'loss': loss,
                     'reward': item.info["reward"], 'returns': item.returns.mean().item(),'advantage':item.advantages.mean().item(), 'values':item.values.mean().item(), 
